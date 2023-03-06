edfd99ecb7f44b70c5fc0a05333566ea4ee54e2c
Noam Amron <noam0574@gmail.com>
2023-03-06 19:15:06 +0200

    anther try to fix socket.io in server

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// const server = http.createServer(app);
+const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
